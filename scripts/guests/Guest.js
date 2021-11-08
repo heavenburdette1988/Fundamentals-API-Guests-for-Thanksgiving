148013e74d4eb64ff2406c1934202464197829fb
heavenburdette1988 <heavenburdette1988@gmail.com>
2021-11-08 13:49:29 -0500

    finished mvp

--- scripts/guests/Guest.js
+++ scripts/guests/Guest.js
@@ -1,10 +1,12 @@
 export const guests = (guestObject) => {
     return `
-    <section class="guestCard"><h2>Current Guests: </h2>
+    <section class="guestCard">
+    <h2>Current Guests: </h2>
     <div id="guestName">Guest Name: ${guestObject.name}</div>
     <div>Age: ${guestObject.age}</div>
     <div>Favorite Dish: ${guestObject.favoriteDish}</div>
-    <div>rightHanded: </div>
-    <img src=${guestObject.imageUrl}>`
+    <div>rightHanded: ${guestObject.rightHanded} </div>
+    <img src=${guestObject.imageUrl}>
+    </section>`
        
 }
--- scripts/guests/GuestForm.js
+++ scripts/guests/GuestForm.js
@@ -12,7 +12,7 @@ document.querySelector("body").addEventListener("click", clickEvent => {
             name: document.querySelector("#guestName").value,
             age: document.querySelector("#guestAge").value,
             favoriteDish: document.querySelector("#favoriteDish").value,
-            rightHanded: document.querySelector("#rightHand").value,
+            rightHanded: document.querySelector("#rightHand").classList.contains("yes") ? true : false ,
             imageUrl: document.querySelector("#image").value
             
         }
@@ -21,8 +21,7 @@ document.querySelector("body").addEventListener("click", clickEvent => {
         document.querySelector("#guestName").value ="",
         document.querySelector("#guestAge").value ="",
         document.querySelector("#favoriteDish").value ="",
-        document.querySelector("#rightHand").value ="",
-        document.querySelector("#leftHand").value =""
+        document.querySelectorAll("#rightHand").value="",
         document.querySelector("#image").value =""
 
         saveGuest(newGuest)
@@ -30,13 +29,37 @@ document.querySelector("body").addEventListener("click", clickEvent => {
     }
 })
 
+// function myFunction() {
+//     // Get the checkbox
+//     var checkBox = document.getElementById("rightHand");
+//     // Get the output text
+//     var text = document.getElementById("text");
+  
+//     // If the checkbox is checked, display the output text
+//     if (checkBox.checked == true){
+//       text.style.display = "block";
+//     } else {
+//       text.style.display = "none";
+//     }
+//   }
 
+contentTarget.addEventListener("click", e => {
+    if(e.target.id === "rightHand"){
+        console.log("sup")
+        document.querySelector("#rightHand").classList.toggle("yes")
+    }
+})
+const righty = () => {
+    return `
+<input type="checkbox" class="" id="rightHand" value="true">
+`
+}
 
 export const guestForm = () => {
     contentTarget.innerHTML = `<article class="GuestForm"><fieldset>Name of Guest: <label for="name"><input type="text" id="guestName"></label></feildset>
     <fieldset>Age of Guest: <input type="text" id="guestAge" placeholder="Input Age Here"></fieldset>
     <fieldset>Guest Favorite Dish:  <input type="text" id="favoriteDish" placeholder="Favorite Dish Here"></fieldset>
-    <fieldset>Are you Righthanded?<input type="checkbox" id="rightHand" value="true">True?<input type="checkbox" id="leftHand" value="false">False?</fieldset>  
+    <fieldset>Are you Righthanded?${righty()}</fieldset>  
     <fieldset>Photo of Guest <input type="url" id="image" placeholder="image URL here" ></fieldset> 
     </article>
         <button id="saveGuest">Add Guest</button>`
